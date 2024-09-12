345020b4240e139889afaaa27cd9e4cfd00f4df3
akashgudase <gudase.akash@gmail.com>
2024-09-12 15:07:15 +0530

    Start of spring mvc

---
+++ springmvc/src/main/java/com/jspiders/springmvc/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.jspiders.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+	@RequestMapping("/login")
+	protected String getLogInPage() {
+		return "login";
+	}
+
+}
